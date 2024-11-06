930a7432766c6afb81e7c662a808cf51904a005f
JirehChen <jche0212@student.monash.edu>
2024-11-06 19:22:11 +1100

    New command-line Connect4 project

---
+++ connect4.py
@@ -0,0 +1,13 @@
+'''
+A quick project: A rudimentary game of Connect 4, run in the terminal, for two players.
+Jireh Chen, 6th Nov 2024
+'''
+
+def runGame():
+    
+    return 0
+
+
+
+if __name__ == "__main__":
+    runGame()
